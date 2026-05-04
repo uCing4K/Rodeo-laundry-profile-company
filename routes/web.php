4efcd1c9e891aad6b61c60b572927cf01cc71786
ArdianDW <ardiandwic37@gmail.com>
2026-05-05 05:10:37 +0700

    update blade routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,39 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+})->name('index');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/tracking', function () {
+    return view('tracking');
+})->name('tracking');
+
+Route::get('/faq', function () {
+    return view('faq');
+})->name('faq');
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', function () {
+        return view('admin.index');
+    })->name('index');
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
 });
+
+Route::get('/welcome', function () {
+    return view('welcome');
+})->name('welcome');
