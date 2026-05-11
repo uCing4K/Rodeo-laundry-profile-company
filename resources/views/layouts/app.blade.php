c89300d30e8e64e5d815cfba713a3c291fd8782a
ArdianDW <ardiandwic37@gmail.com>
2026-05-11 08:06:40 +0700

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>@yield('title', 'Rodeo Laundry - Bersih, Cepat, Terpercaya')</title>
     <meta
       name="description"
@@ -15,6 +16,9 @@
     />
     <meta property="og:type" content="website" />
     <meta property="og:locale" content="id_ID" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
     <link rel="stylesheet" href="{{ asset('asset/css/style.css') }}" />
   </head>
   <body>
